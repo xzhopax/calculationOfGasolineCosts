51dcf1c4d9f3a4ca51dabca92b9ef6c099de272e
calculationOfGasolineCosts <xzhopax@gmail.com>
2020-09-06 15:07:48 +0300

    Saving my project on github (version 1.0) for pure java, without maven and spring. In this project, you can calculate the cost of gasoline when driving around a city or highway, there is a file with the history of calculations and the ability to clear it.

---
+++ src/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"calculationGasoline","l":"Car"},{"p":"calculationGasoline","l":"InCityPanel"},{"p":"calculationGasoline","l":"MenuGUI"},{"p":"calculationGasoline","l":"OnHighwayPanel"},{"p":"calculationGasoline","l":"RunProgram"}]
